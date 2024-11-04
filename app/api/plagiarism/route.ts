7fb6f01ddda86c6dac16762d21a8fbca1755cf54
pratham <vivek.agent47@gmail.com>
2024-11-04 22:37:27 +0530

    lint and api correction

--- app/api/plagiarism/route.ts
+++ app/api/plagiarism/route.ts
@@ -1,42 +1,35 @@
-import type { NextApiRequest } from "next";
 import OpenAI from "openai";
 import { env } from "@/data/env/server";
 
-export async function POST(req: NextApiRequest) {
-  if (req.method === "POST") {
-    const { text } = req.body;
+export async function POST(req: Request) {
+  const { text } = await req.json();
 
-    const openai = new OpenAI({
-      apiKey: env.OPEN_AI_API,
+  const openai = new OpenAI({
+    apiKey: env.OPEN_AI_API,
+  });
+
+  try {
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: text }],
     });
 
-    try {
-      const response = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: text }],
-      });
-
-      return new Response(
-        JSON.stringify({ result: response.choices[0].message.content }),
-        {
-          status: 200,
-          headers: {
-            "Content-Type": "application/json",
-          },
+    return new Response(
+      JSON.stringify({ result: response.choices[0].message.content }),
+      {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
         },
-      );
-    } catch (error: any) {
-      return new Response(
-        JSON.stringify({ result: "Error: " + error.message }),
-        {
-          status: 400,
-          headers: {
-            "Content-Type": "application/json",
-          },
-        },
-      );
-    }
-  } else {
-    throw new Error("Method not allowed");
+      },
+    );
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    return new Response(JSON.stringify({ result: "Error: " + error.message }), {
+      status: 400,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
   }
 }
